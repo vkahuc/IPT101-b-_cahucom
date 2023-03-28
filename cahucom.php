46bc403dc754e4b08dd60d2ccd7f9622bbc164ea
vkahuc <123623723+vkahuc@users.noreply.github.com>
2023-03-28 16:42:46 +0800

    Add files via upload

---
+++ cahucom.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+
+<?php
+        $num = 10;
+
+        echo"The Multiplication of $x <br>";
+        for  ($x = 1; $x <= 10; $x++){
+            
+            $multiple = $x * $num;
+            echo "$num * $x = $multiple <br>";
+        }
+       
+    ?>
+</center>
+    
+</body>
+</html>
